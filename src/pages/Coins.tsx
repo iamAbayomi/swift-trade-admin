2f3b1cfa7785e9d4f7f5009bcbcf0de2ae575d9c
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-08-24 22:06:48 +0100

    added coins and cards method

--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,8 +1,35 @@
-import React from'react'
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import { Link } from "react-router-dom";
+import SetofCards from "../components/SetOfCards";
 
 
-export default class Coins extends React.Component{
+export default class Coins extends React.Component {
     
+    
+    render(){
+        return(
+            <div className="container">
+                <div className="contents">
+                 <Link to="/trade/coin" className="no-text">
+                      <div className="display-flex" style={{float:"right"}}>
+                          <img className="left-arrow" src="/vectors/left-arrow.svg" />
+                          <p>Back to Coin</p>
+                      </div>
+                </Link>
+                  <div className="display-flex clearfix">
+                    <p className="dashboard-title" >Trade</p>
+                  </div>
+                  
+                  <div className="card-white">
+                    <p className="purple-header-typography" >Got Gift Cards For Sale?</p>
+                    <p>Choose your preffered product card to continue the exchange process </p>
+                    <SetofCards/>
+                   </div>
+                </div>
+                
+            </div>
+        )
+    }
+
 }
-
-
