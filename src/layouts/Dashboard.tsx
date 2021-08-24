78252e9f9101e8fa9abc2f224d9b489f6512f951
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-08-24 12:24:12 +0100

    First message

---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,157 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import './Dashboard.css'
+// import Trade from '../pages/Trade'
+// import Coin from '../pages/Coin'
+// import Cards from  '../pages/Cards'
+// import Wallet from '../pages/Wallet'
+// import Transactions from '../pages/Transactions'
+// import Settings from '../pages/Settings'
+// import Overview from '../pages/Overview'
+// import SellCards from '../pages/SellCards'
+// import WalletEmptyState from '../pages/WalletEmptyState'
+
+// import Sell from '../pages/Sell'
+// import BankAccount from '../components/BankAccount'
+// import Profile from '../components/Profile'
+// import Notification from '../components/Notification'
+// import ActivityLog from '../components/Activitylog'
+// import TestPage from '../pages/TestPage'
+// import SettingsRouter from '../pages/SettingsRouter'
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink,
+    // BrowserRouter
+  } from 'react-router-dom';
+
+
+import styled from 'styled-components'
+
+export default class Dashboard extends React.Component{
+
+
+render(){
+    return(
+      <Router>
+        <div className="container">
+            <div className="header-bar">
+                <img className="logo" src="logo.svg" />
+                <div className="header-tool">
+                    <div className="search-bar">
+                        <div className="search-content">
+                            <div className="search-logo-and-highlight">
+                            <img className="search-logo" src="/vectors/search-icon.svg"/>
+                            <input className="search-highlight" 
+                            placeholder="Search e.g card"
+                            />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="profile-section">
+                        <img className="profile-image" src="/vectors/profile-image.svg" />
+                        <p className="username">
+                            Ramon
+                        </p>
+                    </div>
+                    <img className="notifications-icon" src="/vectors/notifications.svg" />
+                </div>
+            </div>
+            <div className="content">
+                <div className="side-menu">
+                    <div className="menu-items-content">
+                        <NavLink to="/overview" className="menu-item" activeClassName="selected">
+                            <img className="menu-icon" src="/vectors/overview.svg" alt="" />
+                            <p className="menu-title">
+                                Overview
+                            </p>
+                        </NavLink>
+
+                        <NavLink to="/trade" className="menu-item" activeClassName="selected">
+                            <img className="menu-icon" src="/vectors/trade.svg" alt="" />
+                            <p className="menu-title">
+                                Trade
+                            </p>
+                        </NavLink>
+
+                        <NavLink to="/wallet" className="menu-item "activeClassName="selected">
+                            <img className="menu-icon" src="/vectors/wallet.svg" alt="" />
+                            <p className="menu-title">
+                                Wallet
+                            </p>
+                        </NavLink>
+
+                        <NavLink to="/transactions" className="menu-item " activeClassName="selected">
+                            <img className="menu-icon" src="/vectors/transactions.svg" alt="" />
+                            <p className="menu-title">
+                                Transactions
+                            </p>
+                        </NavLink>
+
+                        <NavLink  to="/settings/profile" className="menu-item " activeClassName="selected">
+                            <img className="menu-icon" src="/vectors/settings.svg" alt="" />
+                            <p className="menu-title">
+                                Settings
+                            </p>
+                        </NavLink>
+
+                        <NavLink to="/logout" className="menu-item " activeClassName="selected">
+                            <img className="menu-icon" src="/vectors/logout.svg" alt="" />
+                            <p className="menu-title">
+                                Logout
+                            </p>
+                        </NavLink>
+                    </div>
+                </div>
+                <div className="main-section">
+                    <Switch>
+                        {/* <Route exact path ="/" component={Overview} />
+                        <Route path ="/overview" component={Overview} />
+                        <Route path="/trade/coin/sell"  component={Sell} />
+                        <Route exact path="/trade"  component={Coin} />
+                        <Route path="/trade/coin/buy"  component={Sell} />
+                        <Route path="/trade/coin"  component={Coin} />
+                        <Route path="/test/page" component={TestPage} /> */}
+                        {/* <Route exact path="/pendingwallet" component={} />
+                        <Route exact path="/approvedwallet" component={} />
+                        <Route exact path ="/pendingtransfer" component={PendingTransfer} />
+                        <Route exact path="/approved" component={} />
+                        <Route exact path ="/makepayment" component={} />
+                        <Route exact path ="/makepayment3" component={} />
+                        <Route exact path ="/makepayment4" component={} /> */}
+                        {/* <Route exact path="/pendingcards1" component={SellCards} />
+                        <Route exact path="/pendingcards2" component={SellCards} /> */}
+                        {/* <Route exact path="/trade/cards"  component={Cards} />
+                        <Route exact path="/trade/cards2" component={SellCards} />
+                        <Route exact path="/wallet/emptystate" component={WalletEmptyState} />
+                        <Route exact path="/wallet" component={Wallet} />
+                        <Route exact path="/transactions" component={Transactions} /> */}
+                        {/* <Route exact path="/settings/profile" component={Settings} /> */}
+                        {/* <Route exact path="/settings/:slug" component={Settings} /> */}
+                        {/* <Route exact path="/settings" component={Settings} /> */}
+                        {/* <Route path="/settings" component={Settings} /> */}
+                        {/* <Route exact path="/settings/bankaccount" component={BankAccount} /> */}
+                        {/* <Route exact path="/settings/profile" component={Profile} />
+                        <Route exact path="/settings/bankaccount" component={BankAccount} />
+                        <Route exact path="/settings/notification" component={Notification} />
+                        <Route exact path="/settings/activity" component={ActivityLog} /> */}
+
+                    </Switch>
+                    <ImgButton src="/vectors/message-button.svg" />
+                </div>
+            </div>
+        </div>
+        </Router>
+    )
+}
+
+}
+
+
+const ImgButton = styled.img `
+    position: fixed;
+    bottom: 50px;
+    right: 30px;
+`
---
+++ src/pages/Overview.tsx
@@ -0,0 +1,62 @@
+// /* eslint-disable jsx-a11y/alt-text */
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import styled from 'styled-components'
+// // import CollectionofCards from '../components/CollectionofCards'
+// // import ConversionRate from '../components/ConversionRate'
+// // import EmptyStateTrasanctionCards from '../components/EmptyStateTransactionCard'
+// // import SingleTransactionCardView from '../components/SingleTransactionCardview'
+// // import TransactionCards from '../components/TransactionCard'
+
+// // import WalletBalanceCard from '../components/WalletBalanceCard'
+// import './Overview.css'
+
+// export default class Overview extends React.Component{
+
+//     state={
+//         isEmpty : 'yes'
+//     }
+
+//     render(){
+//         return(
+//             <div className="container">
+//               <div className="contents" >
+//                     <p className="username-intro">HELLO, RAMON RIDWAN</p>
+//                     <p className="dashboard-title">Overview</p>
+//                     <Link to="/transactions" className="transaction-row display-flex link">
+                        
+//                         <SingleTransactionCardView percentage = {10} transactiontext="Total Number of Transactions" />
+//                         <SingleTransactionCardView percentage = {6} transactiontext="Pending Transactions" />
+//                         <SingleTransactionCardView percentage = {4} transactiontext="Paid Transactions" />
+//                     </Link>
+
+//                     <ConversionRate />
+                    
+//                     <div className="wallet-row display-flex">
+//                         <Link to="/wallet" className="link">
+//                             <WalletBalanceCard showWalletAddress="yes"/>
+//                         </Link>
+//                         <Link to="/trade/coin" className="link">
+//                             <CollectionofCards />
+//                         </Link>
+//                     </div>
+
+//                     <Link to="/transactions" className="link">
+
+//                         {this.state.isEmpty === 'yes' ?
+
+//                         <EmptyStateTrasanctionCards />
+//                         : <TransactionCards/>
+
+//                         }
+//                     </Link> 
+                  
+//               </div>
+
+//             </div>
+//         )
+//     }
+
+// }
+
+
