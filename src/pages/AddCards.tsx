19e349691d0e940c17e1aa7db18428fd18852dd5
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-08-26 18:26:07 +0100

    added addCards pages

--- src/pages/AddCards.tsx
+++ src/pages/AddCards.tsx
@@ -1,6 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-
+import './Overview.css'
 
 export default class AddCards extends React.Component{
-    
+    render(){
+        return(
+            <div className="container">
+                <div className="contents">
+                    <p>Hello</p>
+                    <div className="card-white">
+                        <img className="" src="/vectors/profile-display-container.svg"/>
+                    </div>
+                </div> 
+            </div>
+        )
+    }
 }
