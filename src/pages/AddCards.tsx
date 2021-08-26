691bf9e71569bac1d4fffa311d5186e3f37aafdc
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-08-26 19:41:23 +0100

    added cards button and elements

--- src/pages/AddCards.tsx
+++ src/pages/AddCards.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import './Overview.css'
+import '../components/Profile.css'
 import styled from 'styled-components'
 
 export default class AddCards extends React.Component{
@@ -8,29 +9,29 @@ export default class AddCards extends React.Component{
         return(
             <div className="container">
                 <div className="contents">
-                    <p>Cards</p>
-                    <div className="card-white">
+                    <p className="dashboard-title">Cards</p>
+                    <CardWhite className="card-white">
                         <div className="profile-display-container">
                             <img className="" src="/vectors/profile-display-container.svg"/>
                         </div>
-                        <div className="reset-password-section">
-                            <div className="old-password">
-                                <p>Old Password</p>
-                                <EditField type="password" className="edit-field" placeholder="Enter your old password"/>
+                        <div className="add-card-section">
+                            <div className="card-name">
+                                <p>Card Name</p>
+                                <EditField type="name" className="edit-field" placeholder="Google Play E-code card"/>
                             </div>
 
-                            <div className="new-password">
-                                <p>New Password</p>
-                                <EditField type="password" className="edit-field" placeholder="New password"/>
+                            <div className="card-type">
+                                <p>Card Type</p>
+                                <EditField type="name" className="edit-field" placeholder="USA iTunes E-Code card"/>
                             </div>
 
-                            <div className="confirm-password">
-                                <p>Confirm Password</p>
-                                <EditField type="password" className="edit-field" placeholder="Confirm password"/>
+                            <div className="card-currency">
+                                <p>Card Currency</p>
+                                <EditField type="name" className="edit-field" placeholder="US Dollars (USD)"/>
                             </div>
-                            <button className="blue-button" >Save Changes</button>
+                            <button style={{ margin: "60px auto 60px auto" }} className="blue-button" >Save Changes</button>
                         </div>
-                    </div>
+                    </CardWhite>
                 </div> 
             </div>
         )
@@ -49,4 +50,11 @@ const EditField = styled.input `
     border-color:#c7d3e6;
     border-radius: 4px;
     border-style: solid;
+`
+
+
+const CardWhite = styled.div `
+    padding: 20px 0px 20px 0px;
+    box-sizing: border-box;
+
 `
