fc402c0e123ee5156b299a2840b681696bc8326a
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-08-27 08:29:40 +0100

    added modal form with class

--- src/components/ModalFormWithCards.tsx
+++ src/components/ModalFormWithCards.tsx
@@ -3,14 +3,25 @@ import './ModalForm.css'
 
 
 export default class ModalFormWithCards extends React.Component {
+    modal: React.RefObject<HTMLDivElement>
     
+    constructor(props: {} | Readonly<{}>){
+        super(props)
+        this.modal = React.createRef()
+
+    }
+    
+    toogleModal(){
+        this.modal.current?.classList.toggle('show-modal')
+    }
+
     render(){
         return(
             <div className="modal-container">
                 <p> Hello what do you think </p>
-                <div className="modal show-modal">
+                <div className="modal show-modal" ref={this.modal}>
                     <div className="modal-content">
-                        <span className="close-button">x</span>
+                        <span className="close-button" onClick={this.toogleModal.bind(this)}>x</span>
                         <h1>Hello what are you doing?</h1>
                     </div>
                 </div>
