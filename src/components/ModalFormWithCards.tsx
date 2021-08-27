d34c614e27b0f99d6891c4cad6475aae54139483
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-08-27 04:45:15 +0100

    added card elements

--- src/components/ModalFormWithCards.tsx
+++ src/components/ModalFormWithCards.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import './ModalForm.css'
 
 
 export default class ModalFormWithCards extends React.Component {
+    
     render(){
         return(
-            <div className="modal">
-                <div className="modal">
+            <div className="modal-container">
+                <p> Hello what do you think </p>
+                <div className="modal show-modal">
                     <div className="modal-content">
                         <span className="close-button">x</span>
                         <h1>Hello what are you doing?</h1>
