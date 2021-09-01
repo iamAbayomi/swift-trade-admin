32d9380628d413508d3cb9d6d1dd18d46ca56a42
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-09-01 05:26:52 +0100

    feature: added options for the menu

---
+++ src/components/MenuOptions.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+function MenuOptions(){
+    return(
+        <div>
+            <img className="transaction-options" src="/vectors/options-menu.svg" />
+        </div>
+    )
+}
+
+export default MenuOptions
