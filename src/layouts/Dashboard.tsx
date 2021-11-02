c0bb4e6c96189c6d4f5e462a1d8ce8f9494d66cc
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-11-02 23:42:45 +0100

    feature: added user state and isAuthenticated method

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -27,158 +27,196 @@ import AddCards from '../pages/AddCards'
 import AddCoins from '../pages/AddCoins'
 import ModalFormWithCards from '../components/ModalFormWithCards'
 import Form from '../pages/Form'
+import { getUser, getUserData, removeUserData } from '../classes/User'
 
 export default class Dashboard extends React.Component{
 
-    
     state ={
-        isActive: false
+        isActive: false,
+        user: {
+            id: '',
+            first_name: '',
+            last_name: '',
+            profile_image: ''
+        },
+        temptoken: ''
     }
+
+    componentDidMount(){
+        this.getPathName()
+    }
+
+    getPathName(){
+        let pathname = window.location.pathname
+        if(pathname.startsWith('/token/')){
+            let token = window.location.pathname.replace('/token/','')
+            getUserData(this.state.user, token)
+        }else{
+            this.isUserAuthenticated()
+        }
+    }
+
 
     showDashboard(){
         this.setState({ isActive : !this.state.isActive })
     }
 
-render(){
-    return(
-      <Router>
-        <div className="container">
-            <div className="header-bar">
-                <img className="menu-toggle" id="toggle-menu" 
-                        src="/vectors/menu.svg"
-                        onClick={this.showDashboard.bind(this)}
-                    />
-                <img className="logo" src="logo.svg" />
-                <div className="header-tool">
-                    <div className="search-bar">
-                        <div className="search-content">
-                            <div className="search-logo-and-highlight">
-                            <img className="search-logo" src="/vectors/search-icon.svg"/>
-                            <input className="search-highlight" 
-                            placeholder="Search e.g card"
-                            />
+    async isUserAuthenticated(){
+        let tempUser = await getUser()
+        this.setState({user: tempUser})
+        if(this.state.user){
+            console.log('it is safe to access this page')
+            console.log(this.state.user)
+        }else{
+            window.location.href ="https://swift-homepage.netlify.app/"
+        }
+    }
+
+    logOut(){
+        removeUserData('token', 'userData')
+        window.location.href = "/"
+    }
+
+    render(){
+        return(
+        <Router>
+            <div className="container">
+                <div className="header-bar">
+                    <img className="menu-toggle" id="toggle-menu" 
+                            src="/vectors/menu.svg"
+                            onClick={this.showDashboard.bind(this)}
+                        />
+                    <img className="logo" src="logo.svg" />
+                    <div className="header-tool">
+                        <div className="search-bar">
+                            <div className="search-content">
+                                <div className="search-logo-and-highlight">
+                                <img className="search-logo" src="/vectors/search-icon.svg"/>
+                                <input className="search-highlight" 
+                                placeholder="Search e.g card"
+                                />
+                                </div>
                             </div>
                         </div>
+                        <div className="profile-section" style={{display : 'flex'}}>
+                            <img className="profile-image" src="/vectors/profile-image.svg" />
+                            <p className="username">
+                                Ramon Ridwan
+                            </p>
+                        </div>
+                        <img className="notifications-icon" src="/vectors/notifications.svg" />
                     </div>
-                    <div className="profile-section" style={{display : 'flex'}}>
-                        <img className="profile-image" src="/vectors/profile-image.svg" />
-                        <p className="username">
-                            Ramon Ridwan
-                        </p>
+                </div>
+                <div className="content">
+                    <div className={`side-menu ${this.state.isActive ? "is-open" : "" }`}>
+                        <div className="menu-items-content">
+                            <NavLink to="/overview" className="menu-item" activeClassName="selected">
+                                <img className="menu-icon" src="/vectors/overview.svg" alt="" />
+                                <p className="menu-title">
+                                    Overview
+                                </p>
+                            </NavLink>
+
+                            <NavLink to="/users" className="menu-item" activeClassName="selected">
+                                <img className="menu-icon" src="/vectors/trade.svg" alt="" />
+                                <p className="menu-title">
+                                    Users
+                                </p>
+                            </NavLink>
+
+                            <NavLink to="/cards" className="menu-item "activeClassName="selected">
+                                <img className="menu-icon" src="/vectors/wallet.svg" alt="" />
+                                <p className="menu-title">
+                                    Cards
+                                </p>
+                            </NavLink>
+
+                            <NavLink to="/coins" className="menu-item " activeClassName="selected">
+                                <img className="menu-icon" src="/vectors/transactions.svg" alt="" />
+                                <p className="menu-title">
+                                    Coins
+                                </p>
+                            </NavLink>
+
+                            <NavLink  to="/payments" className="menu-item " activeClassName="selected">
+                            <img className="menu-icon" src="/vectors/settings.svg" alt="" />
+                                <p className="menu-title">
+                                    Payments
+                                </p>
+                            </NavLink>
+
+                            <NavLink to="/transactions" className="menu-item " activeClassName="selected">
+                                <img className="menu-icon" src="/vectors/logout.svg" alt="" />
+                                <p className="menu-title">
+                                    Transactions
+                                </p>
+                            </NavLink>
+
+                            <NavLink to="/settings" className="menu-item " activeClassName="selected">
+                                <img className="menu-icon" src="/vectors/logout.svg" alt="" />
+                                <p className="menu-title">
+                                    Settings
+                                </p>
+                            </NavLink>
+
+                            <NavLink to="/logout" className="menu-item " activeClassName="selected">
+                                <img className="menu-icon" src="/vectors/logout.svg" alt="" />
+                                <p className="menu-title">
+                                    Logout.
+                                </p>
+                            </NavLink>
+                        </div>
                     </div>
-                    <img className="notifications-icon" src="/vectors/notifications.svg" />
+                    <div className="main-section">
+                        <div className= "dashboard-contents">
+                            <Switch>
+                                <Route exact path ="/" component={Overview} />
+                                <Route path ="/overview" component={Overview} />
+                                <Route path="/users"  component={Users} />
+                                <Route exact path="/cards"  component={Cards} />
+                                <Route path="/coins"  component={Coins} />
+                                <Route path="/payments"  component={Payments} />
+                                <Route path="/transactions" component={Transactions} />
+                                <Route path="/settings" component={Settings} />
+                                <Route path="/logout" component={Logout} />
+                                <Route path="/addcards" component={AddCards} />
+                                <Route path="/addcoins" component={AddCoins} />
+                                <Route path="/form" component={Form} />
+                                <Route path="/modalform" component={ModalFormWithCards} />
+
+                                {/* <Route exact path="/pendingwallet" component={} />
+                                <Route exact path="/approvedwallet" component={} />
+                                <Route exact path ="/pendingtransfer" component={PendingTransfer} />
+                                <Route exact path="/approved" component={} />
+                                <Route exact path ="/makepayment" component={} />
+                                <Route exact path ="/makepayment3" component={} />
+                                <Route exact path ="/makepayment4" component={} /> */}
+                                {/* <Route exact path="/pendingcards1" component={SellCards} />
+                                <Route exact path="/pendingcards2" component={SellCards} /> */}
+                                {/* <Route exact path="/trade/cards"  component={Cards} />
+                                <Route exact path="/trade/cards2" component={SellCards} />
+                                <Route exact path="/wallet/emptystate" component={WalletEmptyState} />
+                                <Route exact path="/wallet" component={Wallet} />
+                                <Route exact path="/transactions" component={Transactions} /> */}
+                                {/* <Route exact path="/settings/profile" component={Settings} /> */}
+                                {/* <Route exact path="/settings/:slug" component={Settings} /> */}
+                                {/* <Route exact path="/settings" component={Settings} /> */}
+                                {/* <Route path="/settings" component={Settings} /> */}
+                                {/* <Route exact path="/settings/bankaccount" component={BankAccount} /> */}
+                                {/* <Route exact path="/settings/profile" component={Profile} />
+                                <Route exact path="/settings/bankaccount" component={BankAccount} />
+                                <Route exact path="/settings/notification" component={Notification} />
+                                <Route exact path="/settings/activity" component={ActivityLog} /> */}
+
+                            </Switch>
+                            <ImgButton src="/vectors/message-button.svg" />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="content">
-                <div className={`side-menu ${this.state.isActive ? "is-open" : "" }`}>
-                    <div className="menu-items-content">
-                        <NavLink to="/overview" className="menu-item" activeClassName="selected">
-                            <img className="menu-icon" src="/vectors/overview.svg" alt="" />
-                            <p className="menu-title">
-                                Overview
-                            </p>
-                        </NavLink>
-
-                        <NavLink to="/users" className="menu-item" activeClassName="selected">
-                            <img className="menu-icon" src="/vectors/trade.svg" alt="" />
-                            <p className="menu-title">
-                                Users
-                            </p>
-                        </NavLink>
-
-                        <NavLink to="/cards" className="menu-item "activeClassName="selected">
-                            <img className="menu-icon" src="/vectors/wallet.svg" alt="" />
-                            <p className="menu-title">
-                                Cards
-                            </p>
-                        </NavLink>
-
-                        <NavLink to="/coins" className="menu-item " activeClassName="selected">
-                            <img className="menu-icon" src="/vectors/transactions.svg" alt="" />
-                            <p className="menu-title">
-                                Coins
-                            </p>
-                        </NavLink>
-
-                        <NavLink  to="/payments" className="menu-item " activeClassName="selected">
-                          <img className="menu-icon" src="/vectors/settings.svg" alt="" />
-                            <p className="menu-title">
-                                Payments
-                            </p>
-                        </NavLink>
-
-                        <NavLink to="/transactions" className="menu-item " activeClassName="selected">
-                            <img className="menu-icon" src="/vectors/logout.svg" alt="" />
-                            <p className="menu-title">
-                                Transactions
-                            </p>
-                        </NavLink>
-
-                        <NavLink to="/settings" className="menu-item " activeClassName="selected">
-                            <img className="menu-icon" src="/vectors/logout.svg" alt="" />
-                            <p className="menu-title">
-                                Settings
-                            </p>
-                        </NavLink>
-
-                        <NavLink to="/logout" className="menu-item " activeClassName="selected">
-                            <img className="menu-icon" src="/vectors/logout.svg" alt="" />
-                            <p className="menu-title">
-                                Logout.
-                            </p>
-                        </NavLink>
-                    </div>
-                </div>
-                <div className="main-section">
-                    <div className= "dashboard-contents">
-                        <Switch>
-                            <Route exact path ="/" component={Overview} />
-                            <Route path ="/overview" component={Overview} />
-                            <Route path="/users"  component={Users} />
-                            <Route exact path="/cards"  component={Cards} />
-                            <Route path="/coins"  component={Coins} />
-                            <Route path="/payments"  component={Payments} />
-                            <Route path="/transactions" component={Transactions} />
-                            <Route path="/settings" component={Settings} />
-                            <Route path="/logout" component={Logout} />
-                            <Route path="/addcards" component={AddCards} />
-                            <Route path="/addcoins" component={AddCoins} />
-                            <Route path="/form" component={Form} />
-                            <Route path="/modalform" component={ModalFormWithCards} />
-
-                            {/* <Route exact path="/pendingwallet" component={} />
-                            <Route exact path="/approvedwallet" component={} />
-                            <Route exact path ="/pendingtransfer" component={PendingTransfer} />
-                            <Route exact path="/approved" component={} />
-                            <Route exact path ="/makepayment" component={} />
-                            <Route exact path ="/makepayment3" component={} />
-                            <Route exact path ="/makepayment4" component={} /> */}
-                            {/* <Route exact path="/pendingcards1" component={SellCards} />
-                            <Route exact path="/pendingcards2" component={SellCards} /> */}
-                            {/* <Route exact path="/trade/cards"  component={Cards} />
-                            <Route exact path="/trade/cards2" component={SellCards} />
-                            <Route exact path="/wallet/emptystate" component={WalletEmptyState} />
-                            <Route exact path="/wallet" component={Wallet} />
-                            <Route exact path="/transactions" component={Transactions} /> */}
-                            {/* <Route exact path="/settings/profile" component={Settings} /> */}
-                            {/* <Route exact path="/settings/:slug" component={Settings} /> */}
-                            {/* <Route exact path="/settings" component={Settings} /> */}
-                            {/* <Route path="/settings" component={Settings} /> */}
-                            {/* <Route exact path="/settings/bankaccount" component={BankAccount} /> */}
-                            {/* <Route exact path="/settings/profile" component={Profile} />
-                            <Route exact path="/settings/bankaccount" component={BankAccount} />
-                            <Route exact path="/settings/notification" component={Notification} />
-                            <Route exact path="/settings/activity" component={ActivityLog} /> */}
-
-                        </Switch>
-                        <ImgButton src="/vectors/message-button.svg" />
-                    </div>
-                </div>
-            </div>
-        </div>
-        </Router>
-    )
-}
+            </Router>
+        )
+    }
 
 }
 
