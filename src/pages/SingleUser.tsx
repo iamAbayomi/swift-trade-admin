30f16e622bf381474bb65fd125789a2cd5c6b274
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-12-27 05:40:41 +0100

    feature: added the user profile image and transaction page

--- src/pages/SingleUser.tsx
+++ src/pages/SingleUser.tsx
@@ -1,11 +1,57 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { useState, useEffect } from "react"
+import styled from "styled-components"
+import SingleTransactionOverview from "../components/SingleTransactionsOverview"
+import TransactionCards from "../components/TransactionCard"
+import TransactionOverview from "../components/TransactionOverview"
+
+type props = {
+    userProfileImage: string,
+    userName: string,
+    userEmail: string
+}
+
 function SingleUser(){
+    const [useProfileImage, setUserProfileImage] = useState("")
+
+    useEffect(()=>{
+        setProfileImage()
+    })
+
+    function setProfileImage(){
+        setUserProfileImage('./vectors/empty-user.png')
+    }
+
     return(
         <div>
-
+            <UserProfileContainer>
+                <img src={useProfileImage}/>
+                <p>Ramon Ridwan</p>
+                <p>Ramonridwan@protonmail.com</p>
+                <button>Inactive</button> <button>Block User</button>
+            </UserProfileContainer>
+            <SingleTransactionContainer>
+                    <SingleTransactionOverview/>
+            </SingleTransactionContainer>
+            <TransactionContainer>
+                <TransactionOverview />
+            </TransactionContainer>
         </div>
     )
 }
 
 export default SingleUser
 
+const UserProfileContainer = styled.div `
+    max-width: 250px;
+    margin: auto;
+`
+const SingleTransactionContainer = styled.div `
+    max-width: 800px;
+    margin: auto;
+`
 
+const TransactionContainer = styled.div `
+    max-width: 1000px;
+    margin: auto;
+`
