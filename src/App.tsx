39741a98321381e8b613298e801e9a1c1d1bf979
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-08 22:54:46 +0100

    fix: change the dashboard and app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Overview from './pages/Overview';
+import Login from './pages/Login';
+import Dashboard from './layouts/Dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div>
+
+        </div>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
