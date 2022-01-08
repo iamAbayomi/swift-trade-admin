01ff08cf2ed4687a45c4358de257841cdeffa336
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-08 08:25:26 +0100

    feature: designed the block user buttons

--- src/pages/SingleUser.tsx
+++ src/pages/SingleUser.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 import styled from "styled-components"
 import { getToken } from "../classes/User"
 import { getUserProfile } from "../classes/Utilities"
+import Chips from "../components/Chips"
 import SingleTransactionCardView from "../components/SingleTransactionCardview"
 import SingleTransactionOverview from "../components/SingleTransactionsOverview"
 import SingleUserTransactionTable from "../components/SingleUserTransactionTable"
@@ -30,7 +31,6 @@ function SingleUser(){
     useEffect(()=>{
         setProfileImage()
         getSingleUserData()
-        getTheUserProfile()
     }, [])
 
     function setProfileImage(){
@@ -46,6 +46,8 @@ function SingleUser(){
                     console.log('This is the data', res)
                     setTransactionCount(res.data.data)
                 }).catch((err) => { console.log(err)})
+            
+            getTheUserProfile(token, userId)
     }
 
     function getUserId(){
@@ -53,15 +55,14 @@ function SingleUser(){
         return userId
     }
 
-    async function getTheUserProfile(){
-        let token = await getToken()
-        let userId = await getUserId()
-        //let userProfileData = await getUserProfile(token, userId)
-        getUserProfile(token, userId)
-            .then((data: any) => {
-                console.log('This is the data ')
-                setUserProfile(data[0])
-            })
+    async function getTheUserProfile(token: any, userId: any){
+        axios.get(`https://swift-trade-v1.herokuapp.com/api/v1/user/${userId}/info`,
+            {headers: {'Authorization' : `Bearer ${token}`}} )
+            .then((res: any)=> {
+                console.log('This is the data', res)
+                    setUserProfile(res.data.data)
+                }).catch((err) => { console.log(err)})
+
         console.log("This is the user profile",  userProfile)
         console.log("This is the user ", getUserProfile(token, userId))
     }
@@ -75,27 +76,30 @@ function SingleUser(){
                 {
                     userProfile != null ? 
                     <div>
-                        <img src={userProfile.profile_picture} className="userprofile profile-settings-image"/>
+                        <img src={userProfile.profile_picture!=null ? userProfile.profile_picture : '/vectors/empty-user.png'} className="userprofile profile-settings-image"/>
                         <p className="userprofile-name">{userProfile.first_name+ " " + userProfile.last_name} </p>
                         <p className="userprofile-email">{userProfile.email}</p>
                     </div>
                     :
-                     <div>
-                         {/* <img src={userProfileImage} className="userprofile"/>
-                        <p>Ramon Ridwan</p>
-                        <p>Ramonridwan@protonmail.com</p> */}
-                     </div>
-
+                     <div/>
                 }
-                {/* <Chips userId={""} chipsText={}  /> <button>Block User</button> */}
+                { userProfile.is_suspended != undefined ?
+                    <StatusBlock className="display-flex">
+                        <Chips userId={null} chipsText={userProfile.is_suspended.toString()} backgroundColor="" /> 
+                        <BlockButton className="chips">
+                            <BlockButtonContents>Block User </BlockButtonContents>
+                        </BlockButton>
+                    </StatusBlock>
+                    : <div/>
+                }
+                
             </UserProfileContainer>
-            
+            {/* This is responsible for displaying the transaction count */}
             <SingleTransactionContainer className="transaction-row display-flex link">
                 <SingleTransactionCardView transactiontext="Total Number of Users" percentage = {transactionCount.successfulTransactions}  />
                 <SingleTransactionCardView transactiontext="Pending Transactions" percentage = {transactionCount.pendingTransactions} />
                 <SingleTransactionCardView transactiontext="Total Number of Transactions" percentage = {transactionCount.allTransactions} />
             </SingleTransactionContainer>
-
             <TransactionContainer>
                 {/* This getUserId sends the user id to the singleUser transaction table and displays it */}
                 <SingleUserTransactionTable userId={getUserId()} />
@@ -118,4 +122,17 @@ const SingleTransactionContainer = styled.div `
 const TransactionContainer = styled.div `
     max-width: 1000px;
     margin: auto;
+`
+
+const StatusBlock = styled.div `
+    max-width: 260px;
+`
+
+const BlockButton = styled.div `
+    color: white;
+    background: #010066;
+`
+
+const BlockButtonContents = styled.p`
+    margin: 5px 5px 5px 15px;
 `
