b4b1f26a93cdf82c433f66412c44d6145da86154
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-09 00:38:08 +0100

    feature: added elements for login to user page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,26 +1,38 @@
 import BlueButton from "../../components/ui-components/Buttons/BlueButton"
+import CustomizeButton from "../../components/ui-components/Buttons/CustomizeButton"
+import './Login.css'
 
 function Login(){
     return(
         <div>
-            <div className="header-section">
-                <img className="logo" src="logo.svg" />  
+            <div className="header-section ">
+                <img className="logo-in-login" src="logo.svg" />  
             </div>
-            <div className="login-section">
-                <input 
-                    className="login-button" 
-                    name="name"
-                    placeholder="Email Address"
+            <div className="body-section">
+                <div className="login-section">
+                    <p>Admin Login</p>
+                    <input 
+                        className="login-button login-edit-field" 
+                        name="name"
+                        placeholder="Email Address"
+                        />
+                    <input 
+                        className="login-button login-edit-field" 
+                        name="name"
+                        placeholder="Password"
+                        />
+                    
+                    <CustomizeButton 
+                        width={"320px"} 
+                        height={"50px"} 
+                        color={"white"} 
+                        backgroundColor={"#010066"} 
+                        buttonText={"Login"} 
                     />
-                <input 
-                    className="login-button" 
-                    name="name"
-                    placeholder="Password"
-                    />
-                <BlueButton buttonText={"Login"} />
-            </div>
-            <div className="icon-section">
-            <img className="value-proposition-icon" src="./vectors/vp-vector.svg" />
+                </div>
+                <div className="icon-section">
+                    <img className="value-proposition-icon" src="./vectors/vp-vector.svg" />
+                </div>
             </div>
         </div>
     )
