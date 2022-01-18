f5ba144d3cfd778825862c8f12aa5025fa0185ed
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-18 23:25:26 +0100

    feature: removed unneccessary code

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,19 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 import { getToken } from "../classes/User"
 import { baseUrl } from "../classes/Utilities"
-import { fetchUsers } from "../reducers/UsersSlice"
+import { fetchUsers, showCurrentUsers, showUsers } from "../reducers/UsersSlice"
 import useAxios from "../classes/CustomAxiosHook"
 
-const showUsers = (state: any ) => state
 
 const TestPage: React.FC = () => {
     const dispatch = useDispatch()
-    const users: any = useSelector<any[]>(showUsers)
+    const users: any = useSelector<any[]>(showCurrentUsers)
+    const [userProfile, setUserProfile] = useState<any>({})
 
     function showUsersFromState(){
-        console.log('This is the value in the usesr state', users)
+        console.log('This is the value in the user state', users)
     }
 
     useEffect(()=>{
@@ -27,7 +27,7 @@ const TestPage: React.FC = () => {
                 users ? <p>{JSON.stringify(users)} </p>
                 : <div/>
             }
-            <p>adda</p>
+
         </div>
     )
 }
