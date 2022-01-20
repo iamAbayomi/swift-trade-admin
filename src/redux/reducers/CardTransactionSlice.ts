f2321325ca887306f3b192ffff23a3eff2c49e01
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-20 08:16:54 +0100

    feature: added all actions to card and coin transactions

--- src/redux/reducers/CardTransactionSlice.ts
+++ src/redux/reducers/CardTransactionSlice.ts
@@ -1,1 +1,24 @@
-export {}
+import { createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
+import { useCustomAxios } from "../../classes/Utilities";
+
+const cardTradeAdapter = createEntityAdapter()
+
+const initialState = cardTradeAdapter.getInitialState()
+
+//
+export const createCard = createAsyncThunk('cards/createCard',async(body)=>{
+    const {response, error} = await useCustomAxios('POST', 'trade/card', body)
+    return response.data.data
+})
+
+export const fetchSingleCard = createAsyncThunk('cards/fetchCard', async(cardId)=>{
+    const {response, error } = await useCustomAxios('GET',`trade/${cardId}/card`)
+    return response.data.data
+})
+
+
+export const fetchAllCard = createAsyncThunk('cards/fetchCard', async()=>{
+    const {response, error } = await useCustomAxios('GET','trade/card')
+    return response.data.data
+})
+
--- src/redux/reducers/CoinTransactionSlice.ts
+++ src/redux/reducers/CoinTransactionSlice.ts
@@ -1,1 +1,24 @@
-export {}
+import { createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
+import { useCustomAxios } from "../../classes/Utilities";
+
+const coinTradeAdapter = createEntityAdapter()
+
+const initialState = coinTradeAdapter.getInitialState()
+
+//
+export const createCoin = createAsyncThunk('coins/createCoin',async(body)=>{
+    const {response, error} = await useCustomAxios('POST', 'trade/coin', body)
+    return response.data.data
+})
+
+export const fetchSingleCoin = createAsyncThunk('coins/fetchCoin', async(coinId)=>{
+    const {response, error } = await useCustomAxios('GET',`trade/${coinId}/coin`)
+    return response.data.data
+})
+
+
+export const fetchAllCoin = createAsyncThunk('coins/fetchCoin', async()=>{
+    const {response, error } = await useCustomAxios('GET','trade/coin')
+    return response.data.data
+})
+
