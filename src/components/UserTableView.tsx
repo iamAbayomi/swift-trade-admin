e7bdaabca4d5e201b974b3bdb89ebe8d9960687e
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-20 11:48:08 +0100

    feature: changed state to redux

--- src/components/UserTableView.tsx
+++ src/components/UserTableView.tsx
@@ -16,22 +16,10 @@ import { fetchAllUser, selectAllUsers } from '../redux/reducers/UsersSlice';
 let history
 
 function UserTableView (){
-    const [userProfileRow, setUserProfileRow] = useState<string[][]>([])
-    const 
-    history = useHistory()
+    const history = useHistory()
 
     const columns = ["Account Name", "User Name", "Email", "Action"]
     const allUsers : any = useSelector<any[]>(selectAllUsers)
-    // const data: any =  []
-    
-    const data: any[][] = [
-        // [<Chips chipsText="Active" backgroundColor="rgba(93, 248, 136, 1)" />,  <UserImageandName image="/vectors/profile-bman-image.svg" name="Ramon Ridwan"/>,"Ramonridwan@protonmail.com", <MenuOptions />],
-        // [<Chips chipsText="Block" backgroundColor="rgba(255, 73, 73, 1)" />, <UserImageandName image="/vectors/profile-woman-image.svg" name="Ramon Ridwan"/>, "Ramonridwan@protonmail.com", <MenuOptions />],
-        // [<Chips chipsText="Active" backgroundColor="rgba(93, 248, 136, 1)" />, <UserImageandName image="/vectors/profile-bman-image.svg" name="Ramon Ridwan"/>, "Ramonridwan@protonmail.com", <MenuOptions />],
-        // [<Chips chipsText="Pending" backgroundColor="rgba(130, 130, 130, 1)" />, <UserImageandName image="/vectors/profile-wman-image.svg" name="Ramon Ridwan"/>, "Ramonridwan@protonmail.com", <MenuOptions />],
-        // [<Chips chipsText="Block" backgroundColor="rgba(255, 73, 73, 1)"/>, <UserImageandName image="/vectors/profile-woman-image.svg" name="Ramon Ridwan"/>, "Ramonridwan@protonmail.com", <MenuOptions />]
-    ]
-    
     
     const options : muiTableOptionType = {
         elevation: 0,
@@ -41,81 +29,34 @@ function UserTableView (){
     
     console.log('all users', allUsers)
     
-    
-    useEffect(()=>{
-        getUsers()
-    }, [])
-
-    // Added method to get the list of users from API
-    async function  getUsers() {
-        
-        let token = await getToken()
-        axios.get('https://swift-trade-v1.herokuapp.com/api/v1/user/users/fetch?limit=20',{
-            headers: {'Authorization' : `Bearer ${token}`}} )
-        .then((res: any)=>{
-                console.log('This is the data', res)
-                setUsersData(res.data.data)
-            })
-            .catch((err) => {
-                // console.log(err.response.data.message)
-                console.log(err)
-            })
-    }
-
-    function setUsersData (userData: any){
-        //This sets the properites needed to display the user data from the API
-        userData.map((item:any) => (
-            // data.push(item.is_suspended, item.profile_picture, item.first_name + item.last_name, item.email )
-            data.push([<Chips userId={item.id} chipsText={item.is_suspended.toString()} backgroundColor="rgba(93, 248, 136, 1)" />,  <UserImageandName image={item.profile_picture} name={item.first_name + " " + item.last_name} />, item.email, <MenuOptions />])
+    //This sets the properites needed to display the user data from the API
+    const dataTables = allUsers.map((item:any) => (
+            [<Chips userId={item.id} chipsText={item.is_suspended.toString()} backgroundColor="rgba(93, 248, 136, 1)" />,  
+                <UserImageandName image={item.profile_picture} name={item.first_name + " " + item.last_name} />, item.email, <MenuOptions />]
         ))
 
-        setUserProfileRow(data)
-    }
-
     
-
     function viewUser( rowData: any){
         console.log(rowData[0].props.userId)
-        
-        // window.location.href = "/users/singleuser/" + rowData[0].props.userId
         history.push("/users/singleuser/" + rowData[0].props.userId)
-        // console.log('this is the props. ', this.props)
     }
     
 
-
-
-
-    
         return(
             <div className="margin-top">
                 <div className="transaction-board card-white margin-top">
                         <p className="transaction-text">Users</p>
-                        {/* <div className="transaction-head-table transaction-ids display-flex">
-                            <p>Date</p>
-                            <p>Transaction ID</p>
-                            <p>Type</p>
-                            <p>Value</p>
-                            <p>Status</p>
-                        </div> */}     
                         <MUIDataTable 
                             title={""}             
-                            data={userProfileRow}
+                            data={dataTables}
                             columns={columns}
                             options = {options}
                             />
-                        {/* <Button className="blue-button" >View More </Button> */}
                 </div>
             </div>
         )
     
 }
-
-// function viewUser( rowData: any){
-//     console.log(rowData[0].props.userId)
-//     window.location.href = "/users/singleuser/" + rowData[0].props.userId
-    
-// }
 
 
 
