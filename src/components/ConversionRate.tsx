41ae01f2565f8225337c0adbd325f999a0758fd6
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-23 15:58:13 +0100

    refactor: removed redundant API call added redux

--- src/components/ConversionRate.tsx
+++ src/components/ConversionRate.tsx
@@ -12,23 +12,37 @@ import { useAppSelector } from '../redux/hooks';
 import { selectAllCards } from '../redux/reducers/CardsSlice';
 import { selectAllCoins } from '../redux/reducers/CoinsSlice';
 
-const data: (string[] | object[] | number[])[]= []
+let data: (string[] | object[] | number[])[]= []
 
-function ConversionRate() {
+function ConversionRate () {
+
     const card = useAppSelector(selectAllCards)
     const coin = useAppSelector(selectAllCoins)
-    console.log("This is the card response ", card)
 
-    setConverstionRate(card, coin)
+    console.log('Array of cards ', card)
+
+    const getMuiTheme = () => createTheme({
+        overrides:{
+            MuiTableCell: { root :{ borderBottom: "none"}},
+            MuiPaper :{
+                root:{
+                    width: "100%"   
+                }
+            }
+        }
+    })
+
+    data = setConverstionRate(card, coin)
     function setConverstionRate(card: any, coin: any){
+        const tempData : any[][] = []
         card.map((item: any) => {
-            data.push([item.name," " ,item.rate])
+            tempData.push([item.name," " ,item.rate])
         })
 
         coin.map((item: any) => {
-            data.push([item.name, " " , item.rate])
+            tempData.push([item.name, " " , item.rate])
         })
-   
+        return tempData
     }
 
         return(
@@ -40,38 +54,44 @@ function ConversionRate() {
                         </div>
                         <ClearFix>
                             <div className="display-flex-withoutspace">    
-                                
+                                <MuiThemeProvider theme={getMuiTheme()}>
                                     <MUIDataTable
                                         title={""}    
                                         data={data}
                                         columns={columns}
                                         options = {options}
                                     />
-                                
+                                </MuiThemeProvider>
                                 <img className="forward-button" src="./vectors/forward-button.svg" />
                             </div>
                         </ClearFix>
                     </div>
                 </div>
             )
-    }
-
+}
 
 export default ConversionRate
 
+// const columns = ["iTunes Card", "", "Amazon Card", "" ,"Bitcoin", ""]
+
 const columns = ["Card", " " ,"Rate"]
+
 
 const options : muiTableOptionType = {
     elevation: 0,
     rowsPerPage: 5,
     responsive: 'standard'
 }
+
 const TransactionOptions = styled.img `
     margin: 10px 0px 20px 10px;
 
 `
+
+
 const ClearFix = styled.div `
     clear:both;
 `
+
 
 
