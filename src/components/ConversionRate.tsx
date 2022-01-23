3a3cf4c92dbe83af7e1ea92e7651d6543a2ee4d7
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-23 15:29:30 +0100

    feature: reduce app size

--- src/components/ConversionRate.tsx
+++ src/components/ConversionRate.tsx
@@ -8,62 +8,19 @@ import {createTheme} from '@material-ui/core/styles'
 import { getToken } from '../classes/User';
 import axios from 'axios';
 import { muiTableOptionType } from '../classes/Utilities';
+import { useAppSelector } from '../redux/hooks';
+import { selectAllCards } from '../redux/reducers/CardsSlice';
+import { selectAllCoins } from '../redux/reducers/CoinsSlice';
 
 const data: (string[] | object[] | number[])[]= []
 
-export default class ConversionRate extends React.Component {
+function ConversionRate() {
+    const card = useAppSelector(selectAllCards)
+    const coin = useAppSelector(selectAllCoins)
+    console.log("This is the card response ", card)
 
-    getMuiTheme = () => createTheme({
-        overrides:{
-            MuiTableCell: {
-                root :{
-                    borderBottom: "none"
-                }
-            },
-            MuiPaper :{
-                root:{
-                    width: "100%"   
-                }
-            }
-        }
-    })
-
-    state = {
-        coin: "",
-        card: "",
-        conversionRateData: []
-    }
-
-    componentDidMount(){
-        this.getCards()
-    }
-
-    async getCards(){
-        let token = await getToken()
-        axios.get('https://swift-trade-v1.herokuapp.com/api/v1/cards', {
-             headers: {'Authorization' : `Bearer ${token}`
-            }}).then((res: any) => {
-                this.setState( { card: res.data.data})
-                this.getCoins( this.state.card)
-                console.log('this is the response of the cards', res)
-            })
-            .catch((err)=>{console.log(err)})
-    }
-    
-    async getCoins( card : any){
-        let token = await getToken()
-        axios.get('https://swift-trade-v1.herokuapp.com/api/v1/coins',{
-            headers:{'Authorization' : `Bearer ${token}`}})
-            .then((res: any) => {
-                this.setState({coin: res.data.data})
-                this.setConverstionRate(card , this.state.coin)
-                console.log('This is the response', res)
-            })
-            .catch((err)=>{console.log(err)})
-        
-    }
-
-    setConverstionRate(card: any, coin: any){
+    setConverstionRate(card, coin)
+    function setConverstionRate(card: any, coin: any){
         card.map((item: any) => {
             data.push([item.name," " ,item.rate])
         })
@@ -71,15 +28,9 @@ export default class ConversionRate extends React.Component {
         coin.map((item: any) => {
             data.push([item.name, " " , item.rate])
         })
-
-        this.setState({conversionRateData : data})
+   
     }
 
-
-
-
-    
-    render(){
         return(
                 <div className="conversion-board card-white">
                     <div className="">
@@ -89,14 +40,14 @@ export default class ConversionRate extends React.Component {
                         </div>
                         <ClearFix>
                             <div className="display-flex-withoutspace">    
-                                <MuiThemeProvider theme={this.getMuiTheme()}>
+                                
                                     <MUIDataTable
                                         title={""}    
-                                        data={this.state.conversionRateData}
+                                        data={data}
                                         columns={columns}
                                         options = {options}
                                     />
-                                </MuiThemeProvider>
+                                
                                 <img className="forward-button" src="./vectors/forward-button.svg" />
                             </div>
                         </ClearFix>
@@ -104,31 +55,21 @@ export default class ConversionRate extends React.Component {
                 </div>
             )
     }
-}
 
-// const columns = ["iTunes Card", "", "Amazon Card", "" ,"Bitcoin", ""]
+
+export default ConversionRate
 
 const columns = ["Card", " " ,"Rate"]
-
-// const data = [
-//     ["USA Physical",   "N400.00", "USA Physical",   "N400.00", "Bitcoin BTC",  " N400.00"],
-//     ["USA E-Code Card",   "N400.00", "USA E-Code Card",   "N400.00", "Bitcoin BTC",   "N400.00"],
-//     ["UK Physical Card",    "N400.00", "UK Physical Card",   "N400.00", "Bitcoin BTC",   "N400.00"],
-//     ["UK E-Code Card",   "N400.00", "UK E-Code Card",   "N400.00", "Bitcoin BTC",   "N400.00"]
-// ]
 
 const options : muiTableOptionType = {
     elevation: 0,
     rowsPerPage: 5,
     responsive: 'standard'
 }
-
 const TransactionOptions = styled.img `
     margin: 10px 0px 20px 10px;
 
 `
-
-
 const ClearFix = styled.div `
     clear:both;
 `
