7e074aa61712112aac25868fbd59678ac440c3b7
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-01-23 15:30:11 +0100

    featue: added card and coin reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit"
 import usersReducer from './reducers/UsersSlice'
 import transactionsReducer from './reducers/TransactionsSlice'
+import cardReducer from './reducers/CardsSlice'
+import coinReducer from './reducers/CoinsSlice'
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
-        transactions: transactionsReducer
+        transactions: transactionsReducer,
+        cards: cardReducer,
+        coins: coinReducer
     }
 })
 
